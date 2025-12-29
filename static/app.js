a253507281af86f9ead7a34e504d7057e71c0754
teymuri-alireza <kishijung@gmail.com>
2025-12-29 21:16:23 +0330

    Re-style and re-write functions for the projects section

--- static/app.js
+++ static/app.js
@@ -1,26 +1,19 @@
-let content = document.getElementById("desc-content");
+let intro = document.getElementById("content-intro");
+let projects = document.getElementById("content-projects");
+projects.style.transition = "opacity 0.3s ease";
+intro.style.transition = "opacity 0.3s ease";
 
 function showIntro() {
-    content.style.transition = "opacity 0.3s ease";
-    content.style.opacity = "0";
+    projects.style.opacity = "0";
     setTimeout(() => {
-        introText = `<p>Hello, I'm Alireza Teymuri - a web developer based in Iran.
-                    I enjoy building back-end systems, solving CTF challenges
-                    and contributing to open-source projects on GitHub. When I'm not coding,
-                    you'll probably find me playing chess.</p>
-                    <p>Coffee is my daily debugger, and Linux is my engine.</p>`;
-        content.innerHTML = introText;
-        content.style.opacity = "1";
+        intro.style.opacity = "1";
     }, 150);
 }
 
 function showProjects() {
-    content.style.transition = "opacity 0.3s ease";
-    content.style.opacity = "0";
+    intro.style.opacity = "0";
     setTimeout(() => {
-        projectsText = `The projects show be written here`;
-        content.innerHTML = projectsText;
-        content.style.opacity = "1";
+        projects.style.opacity = "1";
     }, 150);
 }
 
